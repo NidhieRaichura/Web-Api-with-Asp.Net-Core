0a9c42d43d5d63bcb36f132dcd88f79d41b50184
NidhieRaichura <nidhie.raichura@gmail.com>
2019-02-15 23:49:08 +0530

    Add DB context from Azure SQL Server DB to Services

--- H_Plus_Sports/H_Plus_Sports/Startup.cs
+++ H_Plus_Sports/H_Plus_Sports/Startup.cs
@@ -28,6 +28,10 @@ namespace H_Plus_Sports
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Add DB context from Azure SQL DB to Services
+            var connection = "Server=tcp:hsportsraichura.database.windows.net,1433;Initial Catalog=H_Plus_Sports;Persist Security Info=False;User ID=Nidhie;Password=Localhost:4200;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            services.AddDbContext<H_Plus_SportsContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
